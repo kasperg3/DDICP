507487bb3989b49721e6eb8e7fcaf5f49f018cfa
kasperg3 <kasperg3@rocketmail.com>
2024-09-18 14:34:56 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,190 @@
+import json
+
+import contextily as cx
+import matplotlib.pyplot as plt
+import shapely
+import trajgenpy.Logging
+from trajgenpy import Utils
+from trajgenpy.Geometries import (
+    GeoMultiPolygon,
+    GeoMultiTrajectory,
+    GeoPolygon
+)
+from trajgenpy.Query import query_features
+import numpy as np
+from scipy.ndimage import gaussian_filter
+from shapely.geometry import LineString
+from shapely.ops import transform
+import seaborn as sns
+from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.widgets import Slider
+import matplotlib.colors as mcolors
+
+log = trajgenpy.Logging.get_logger()
+# Function to interpolate points on the line
+def interpolate_line(line, distance):
+    num_points = int(line.length / distance)
+    points = []
+    if num_points != 0:
+        points = [line.interpolate(float(i) / num_points, normalized=True) for i in range(num_points + 1)]
+    else: 
+        points = [line.interpolate(2, normalized=True)]
+    return points
+
+# # Apply the heatmap generation to all road lines
+def generate_heatmap(geometry_collection, sample_distance, xedges, yedges):
+    heatmap = np.zeros((len(xedges) - 1, len(yedges) - 1))
+    for feature in geometry_collection.geoms:
+        line = None
+        if isinstance(feature, LineString):
+            line = feature
+        elif isinstance(feature, shapely.geometry.Polygon):            
+            line = feature.exterior
+            
+        interpolated_points = interpolate_line(line, sample_distance)
+        x, y = zip(*[(point.x, point.y) for point in interpolated_points])
+        
+        temp_heatmap, _, _ = np.histogram2d(x, y, bins=(xedges, yedges))
+        heatmap += temp_heatmap
+    return heatmap
+
+def normalize_heatmap(heatmap, norm):
+    # Normalize or clip the heatmap
+    if norm == "clip":
+        heatmap = np.clip(heatmap, 0, 1)
+    elif norm== "normalize":
+        heatmap = heatmap / np.max(heatmap)
+    return heatmap
+
+def interactive_plot(polygon_file,norm, use_sliders=True, plot_environment = True):
+    # Load the GeoJSON data
+    with open(polygon_file, "r") as f:
+        data = json.load(f)
+
+    # Extract the polygon coordinates
+    coordinates = data["features"][0]["geometry"]["coordinates"][0]
+    query_region = shapely.Polygon(coordinates)
+    # Create the shapely Polygon object
+    polygon = GeoPolygon(query_region).set_crs("EPSG:2197")
+
+    wetland = query_features(
+        GeoPolygon(query_region),
+        {
+            "natural": ["water", "wetland"],
+        },
+    )
+    wetland_collection = []
+    for feature in wetland.values():
+        wetland_collection.extend(list(feature.geoms))
+    wetland_geom = GeoMultiPolygon(wetland_collection).set_crs("EPSG:2197")
+    
+    roads = query_features(
+        GeoPolygon(query_region),
+        {
+            "highway": ["service", "track"],
+        },
+    )
+    road_collection = []
+    for road in roads.values():
+        road_collection.extend(list(road.geoms))
+    road_geom = GeoMultiTrajectory(road_collection).set_crs("EPSG:2197")
+    
+
+    # Extract the x and y coordinates of the polygon
+
+    # Determine the bounds of the polygon
+    sample_distance = 1
+    minx, miny, maxx, maxy = polygon.geometry.bounds
+
+    # Define the number of bins for the heatmap
+    num_bins = 200
+
+    # Create the edges for the histogram bins
+    xedges = np.linspace(minx-100, maxx+100, num_bins + 1)
+    yedges = np.linspace(miny-100, maxy+100, num_bins + 1)
+
+    # Generate heatmap for road geometries
+    road_heatmap= generate_heatmap(road_geom.geometry, sample_distance, xedges, yedges)
+    wetland_heatmap= generate_heatmap(wetland_geom.geometry, sample_distance, xedges,yedges)
+    
+    heatmaps = {"roads": road_heatmap, "wetland" :wetland_heatmap}
+    # Apply Gaussian filter to the combined heatmap
+    heatmap = gaussian_filter(road_heatmap + wetland_heatmap, sigma=3)
+    heatmap = normalize_heatmap(heatmap, norm)
+    # Create a figure and axis for the slider
+    fig, ax = plt.subplots()
+    plt.subplots_adjust(left=0.25, bottom=0.25)
+
+
+    if plot_environment:
+        # Ploting 
+        # road_geom.plot(color="red")
+        # wetland_geom.plot()
+        polygon.plot(facecolor="none", edgecolor="black", linewidth=2)
+        # polygon.buffer(50).plot(
+        #     facecolor="none",
+        #     edgecolor="red",
+        #     linewidth=2,
+        #     linestyle="dashed",
+        # )
+        alpha = 0.4
+        Utils.plot_basemap(provider=cx.providers.OpenStreetMap.Mapnik, crs="EPSG:2197")
+        # colorscheme
+        # Flare
+        colors = [(1, 1, 1)] + sns.color_palette("flare", as_cmap=False)
+        # JET
+        colors =  [mcolors.to_rgb(c) for c in plt.cm.jet(np.linspace(0, 1, 256))]
+        palette = LinearSegmentedColormap.from_list("custom_flare", colors)
+    else: 
+        alpha = 1
+        palette = "Greys"
+
+    # Use the custom colormap for the heatmap
+    cbar = plt.colorbar(plt.cm.ScalarMappable(cmap=palette), ax=ax, orientation='vertical', fraction=0.046, pad=0.04)
+    cbar.set_label('Target Distribution')
+    extent = [xedges[0], xedges[-1], yedges[0], yedges[-1]]
+    heatmap_img = plt.imshow(heatmap.T, extent=extent, origin='lower', cmap=palette, alpha=alpha)
+    
+
+    if use_sliders:
+        filter_sliders = {}
+        multiplier_sliders = {}
+        for key in heatmaps.keys():
+            y_position = 0.2 - 0.05 * list(heatmaps.keys()).index(key)
+            
+            ax_filter_slider = plt.axes([0.25, y_position, 0.25, 0.03])
+            ax_multiplier_slider = plt.axes([0.55, y_position, 0.25, 0.03])
+            
+            filter_sliders[key] = Slider(ax_filter_slider, f'{key}:  σ', 0.0, 10.0, valinit=3, valstep=0.1)
+            multiplier_sliders[key] = Slider(ax_multiplier_slider, 'α', 0.0, 3.0, valinit=1, valstep=0.1)
+
+        def update(val):
+            combined_heatmap = np.zeros_like(road_heatmap)
+            for key, slider in filter_sliders.items():
+                combined_heatmap += gaussian_filter(heatmaps[key]*multiplier_sliders[key].val, sigma=slider.val)
+            combined_heatmap = normalize_heatmap(combined_heatmap, norm)
+            heatmap_img.set_data(combined_heatmap.T)
+            plt.draw()
+
+        for slider in filter_sliders.values():
+            slider.on_changed(update)
+        for slider in multiplier_sliders.values():
+            slider.on_changed(update)
+ 
+
+    # plt.axis("equal")
+    ax.set_axis_off()
+    # plt.savefig("heatmap_2point.png", dpi=1200, bbox_inches='tight')
+    plt.show()
+
+# TODO Look into checkboxes for enabling and disabling environmental features: https://gist.github.com/DataSolveProblems/143e2c6f5ecd2c0b4876ac4308e7a2d0
+
+if __name__ == "__main__":
+    use_sliders = True
+    plot_environment = True    
+    norm = None # Options: "normalize", "clip", None
+    polygon_file = "DemaScenarios/FlatTerrainNature.geojson"
+    # polygon_file = "DemaScenarios/HillyTerrainNature.geojson"
+    # polygon_file = "DemaScenarios/Urban.geojson"
+    polygon_file = "DemaScenarios/Water.geojson"
+    interactive_plot(polygon_file, norm)
